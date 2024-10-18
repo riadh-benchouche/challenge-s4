ff21d7a775b976cf842b633e783b92b59a1d0c29
riad <riad.ahmedyahia@yahoo.fr>
2024-10-18 18:37:20 +0200

    add services authentification

---
+++ services/auth_service.go
@@ -0,0 +1,77 @@
+package services
+
+import (
+	"backend/database"
+	"backend/models"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+	"time"
+)
+
+type AuthService struct{}
+
+func NewAuthService() *AuthService {
+	return &AuthService{}
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func (s *AuthService) HashPassword(password struing) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 6)
+	return string(bytes), err
+}
+
+func (s *AuthService) CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func (s *AuthService) ValidateToken(tokenString string) (*jwt.Token, error) {
+	jwtSecret, ok := os.LookupEnv("JWT_KEY")
+	if !ok {
+		return nil, errors.ErrInternal
+	}
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecret), nil
+	})
+}
+
+func (s *AuthService) Login(email, password string) (*LoginResponse, error) {
+	var targetUser models.User
+	database.CurrentDatabase.Where("email = ?", email).First(&targetUser)
+
+	if targetUser.ID == 0 {
+		return nil, errors.ErrInvalidCredentials
+	}
+
+	if !s.CheckPasswordHash(password, targetUser.Password) {
+		return nil, errors.ErrInvalidCredentials
+	}
+
+	jwtSecret, ok := os.LookupEnv("JWT_KEY")
+	if !ok {
+		return nil, errors.ErrInternal
+	}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"id":    targetUser.ID,
+			"name":  targetUser.Name,
+			"email": targetUser.Email,
+			"role":  targetUser.Role,
+			"iat":   time.Now().Unix(),
+			"exp":   time.Now().Add(6 * time.Hour).Unix(),
+		},
+	)
+
+	token, err := t.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return nil, errors.ErrInternal
+	}
+
+	return &LoginResponse{Token: token}, nil
+}
